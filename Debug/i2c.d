9d594d552e2cd28c3c17b06539fc6fc66567a830
elec-tra <saranyan.stuttgart@gmail.com>
2021-10-15 10:55:26 +0200

    DS3231 interfaced with microcontroller by I2C

---
+++ Debug/i2c.d
@@ -0,0 +1,10 @@
+i2c.o: ../i2c.c ../common_preprocessors.h ../general_utility_functions.h \
+ ../usart0.h ../i2c.h
+
+../common_preprocessors.h:
+
+../general_utility_functions.h:
+
+../usart0.h:
+
+../i2c.h:
